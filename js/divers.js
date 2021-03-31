56e6e688a92f7778dc38214ee450c697d485e245
Dalkeri <rebelo;dacosta.damien@gmail.com>
2021-03-31 19:46:48 +0200

    ajout heroku

--- js/divers.js
+++ js/divers.js
@@ -1,4 +1,7 @@
-const serverAddress = "http://localhost:3000/api/teddies/";
+const serverAddress = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+                        ? "http://localhost:3000/api/teddies/"
+                        : " https://orinoco-drdc.herokuapp.com/api/teddies/";
+
 let cart = (JSON.parse(localStorage.getItem("OrinocoCartStored")) || [] );
 // let badge = document.getElementById("navbarBadge");
 let navbarBadge = document.querySelector("#navbarBadge");
