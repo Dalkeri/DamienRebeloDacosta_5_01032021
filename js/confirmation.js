e7fdec7918fbfa50967551db254fdb90b29d7807
Dalkeri <rebelo;dacosta.damien@gmail.com>
2021-04-03 12:42:46 +0200

    ajout d'une div au cas ou on va sur la page confirmation sans réponse du serveur dans le localstorage

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,4 +1,13 @@
 let orderConfirmation = (JSON.parse(localStorage.getItem("OrinocoOrderConfirmation")) || [] );
 let orderId = document.getElementById("orderId");
+let OrinocoOrderConfirmationFound = document.getElementById("OrinocoOrderConfirmationFound");
+let OrinocoOrderConfirmationNotFound = document.getElementById("OrinocoOrderConfirmationNotFound");
+OrinocoOrderConfirmationNotFound.style.display = "none";
 
 orderId.textContent = orderConfirmation.orderId;
+
+//si on va sur la page de confirmation sans localstorage de la réponse du serveur
+if(orderConfirmation.length == 0){
+    OrinocoOrderConfirmationNotFound.style.display = "block";
+    OrinocoOrderConfirmationFound.style.display = "none";
+}
